0a6cc5aab0dfc2581d530b94ae20309ee2267562
Poker Trainer <petralbaviciosojq25@gmail.com>
2026-08-13 13:57:57 +0800

    Redesign poker table UI for client-style play

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='poker-trainer-v8';
+const CACHE='poker-trainer-v9';
 const CORE=['./','./index.html','./texas_holdem_trainer.html','./gto_engine.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
